42641891b52e085446ba8aaae14c2a6afb2f76f4
Glitch (scratch-bugle) <none>
2019-04-11 15:44:23 +0000

    🎑🎽 Updated with Glitch

--- server.js
+++ server.js
@@ -5,6 +5,16 @@
 const express = require('express');
 const app = express();
 
+var connected_users = {};
+
+class User{
+  
+  User(name){
+    this.name = name;
+}
+
+
+};
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -16,6 +26,15 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.post('/connect/:user', function(req, res) {
+  
+ var username = req.params.user;
+ connected_users[username] = new User(username);
+  
+  res.json(connected_users);
 });
 
 // listen for requests :)
